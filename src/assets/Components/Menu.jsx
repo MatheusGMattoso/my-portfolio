fe65a8c177c931179e22060903fa902fb0ef9d9a
“MatheusGMattoso” <mgarcia3818@gmail.com>
2024-10-07 14:27:42 -0300

    feat: create menu

--- src/assets/Components/Menu.jsx
+++ src/assets/Components/Menu.jsx
@@ -4,15 +4,15 @@ const Menu = () => {
   return (
     <div className='h-14 bg-black'>
       <div className="menu h-full flex items-center justify-between font-bold">
-        <h1 className='text-purple-400 p-10'>Mattoso</h1>
+        <h1 className='text-purple-400 ml-10 text-2xl'>Mattoso</h1>
         <div className="lista">
           <ul className='flex text-white'>
-            <li className='mr-10 cursor-pointer'><p>Home</p></li>
-            <li className='mr-10 cursor-pointer'><a><p>About this Project</p></a></li>
-            <li className='mr-10 cursor-pointer'><a><p>About Me</p></a></li>
-            <li className='mr-10 cursor-pointer'><a><p>Tecnologies</p></a></li>
-            <li className='mr-10 cursor-pointer'><a><p>Projects</p></a></li>
-            <li className='mr-10 cursor-pointer'><a><p>Contact</p></a></li>
+            <li className='mr-10 cursor-pointer hover:border-purple-400 hover:border-b-2'><p>Home</p></li>
+            <li className='mr-10 cursor-pointer hover:border-purple-400 hover:border-b-2'><a><p>About this Project</p></a></li>
+            <li className='mr-10 cursor-pointer hover:border-purple-400 hover:border-b-2'><a><p>About Me</p></a></li>
+            <li className='mr-10 cursor-pointer hover:border-purple-400 hover:border-b-2'><a><p>Tecnologies</p></a></li>
+            <li className='mr-10 cursor-pointer hover:border-purple-400 hover:border-b-2'><a><p>Projects</p></a></li>
+            <li className='mr-10 cursor-pointer hover:border-purple-400 hover:border-b-2'><a><p>Contact</p></a></li>
           </ul>
         </div>
       </div>
